1d7bcd8aa61405c228d8e04e4b1ca7e4b9e1b77e
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-04 21:52:40 +0300

    problem not foun yet

--- app/src/main/java/com/kumsal/kyk/MainActivity.kt
+++ app/src/main/java/com/kumsal/kyk/MainActivity.kt
@@ -2,11 +2,27 @@ package com.kumsal.kyk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.Toolbar
+
+import androidx.viewpager.widget.ViewPager
+import com.kumsal.kyk.bottomTabs.SectionPagerAdapter
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var toolbar: Toolbar
+    private lateinit var mViewPager: ViewPager
+    private lateinit var sectionPagerAdapter: SectionPagerAdapter
+    private lateinit var mTableLayout: TableLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar=findViewById(R.id.main_appbar)
+        mViewPager=findViewById(R.id.main_activity_pager_view)
+        sectionPagerAdapter= SectionPagerAdapter()
+        mTableLayout=findViewById(R.id.main_activity_tabLayout)
+
+        setSupportActionBar(toolbar)
+
     }
 }
--- app/src/main/java/com/kumsal/kyk/bottomTabs/SectionPagerAdapter.kt
+++ app/src/main/java/com/kumsal/kyk/bottomTabs/SectionPagerAdapter.kt
@@ -4,8 +4,10 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
-class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+class SectionPagerAdapter: FragmentPagerAdapter {
+    constructor(
 
+    )
     override fun getCount(): Int {
         return 4
     }
@@ -36,6 +38,30 @@ class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
            }
        }
 
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position){
+            0->{
+
+                return "Ana Sayfa";
+            }
+            1->{
+
+                return "Mesajlar"
+            }
+            2->{
+
+                return "Arama"
+            }
+            3->{
+                val account:account_fragment=account_fragment()
+                return "Hesap"
+            }
+            else -> {
+                return " "
+            }
+        }
     }
 
 }
