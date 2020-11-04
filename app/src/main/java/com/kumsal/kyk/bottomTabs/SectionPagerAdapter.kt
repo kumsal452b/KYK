bb0fec3a70886de658b3653264c96ec4dadb3925
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-04 21:12:42 +0300

    add new visual desing

---
+++ app/src/main/java/com/kumsal/kyk/bottomTabs/SectionPagerAdapter.kt
@@ -0,0 +1,41 @@
+package com.kumsal.kyk.bottomTabs
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class SectionPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        return 4
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+       return when(position){
+            0->{
+                val home:home_fragment= home_fragment()
+                return home;
+            }
+            1->{
+                val message:message_fragment= message_fragment()
+                return message
+            }
+            2->{
+                val searchf:search_fragment= search_fragment()
+                return searchf
+            }
+            3->{
+                val account:account_fragment=account_fragment()
+                return account
+            }
+
+           else -> {
+               val account:account_fragment=account_fragment()
+               return account
+           }
+       }
+
+    }
+
+}
