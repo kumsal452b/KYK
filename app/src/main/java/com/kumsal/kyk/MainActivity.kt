6f55b6528b156717b381f0e3862aea83d130a7e3
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-07 23:11:51 +0300

    end of day

--- app/src/main/java/com/kumsal/kyk/MainActivity.kt
+++ app/src/main/java/com/kumsal/kyk/MainActivity.kt
@@ -6,10 +6,12 @@ import android.graphics.Color
 import android.os.Bundle
 import android.provider.CalendarContract
 import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 
 import com.google.android.material.tabs.TabLayout;
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -23,6 +25,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sectionPagerAdapter: SectionPagerAdapter
     private lateinit var mBottomBar: BottomBar
     private lateinit var mFloatingActionButton: FloatingActionButton
+    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,10 +36,14 @@ class MainActivity : AppCompatActivity() {
         sectionPagerAdapter = SectionPagerAdapter(supportFragmentManager)
         mBottomBar = findViewById(R.id.main_activity_bottomBar)
         mFloatingActionButton=findViewById(R.id.fab)
+        mDrawerLayout=findViewById(R.id.main_activity_drawer)
 
+        setSupportActionBar(toolbar)
+        actionBarDrawerToggle= ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close)
+        mDrawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
 
         mViewPager.adapter = sectionPagerAdapter
-        setSupportActionBar(toolbar)
         mBottomBar.setBackgroundColor(Color.BLUE)
         mBottomBar.setOnTabSelectListener(OnTabSelectListener { tabId: Int ->
             run {
