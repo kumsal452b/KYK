942a77efce44ef9c6dcc35529c49372633bcdb70
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-15 16:39:05 +0300

    create new UI class

---
+++ app/src/main/java/com/kumsal/kyk/UID.kt
@@ -0,0 +1,10 @@
+package com.kumsal.kyk
+
+class UID {
+   private var uid:String
+        get() = field
+        set(value) { field = value }
+    constructor(uid: String) {
+        this.uid = uid
+    }
+}
