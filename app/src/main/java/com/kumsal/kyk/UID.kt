fc42f52daf785c3b0e354e5c70c735d142441c65
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-15 21:25:31 +0300

    problem has found

--- app/src/main/java/com/kumsal/kyk/UID.kt
+++ app/src/main/java/com/kumsal/kyk/UID.kt
@@ -1,10 +1,23 @@
 package com.kumsal.kyk
 
 class UID {
-   private var uid:String
+    var uid:String
         get() = field
         set(value) { field = value }
+
     constructor(uid: String) {
         this.uid = uid
+    }
+    constructor():this("number"){
+
+    }
+    companion object{
+        private lateinit var instance:UID
+        fun getInstance():UID{
+            if (instance==null){
+                instance= UID()
+            }
+            return instance
+        }
     }
 }
