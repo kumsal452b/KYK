f252f879e29d687853873268081a178496bf7027
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-21 15:56:09 +0300

    edit text change

--- app/src/main/java/com/kumsal/kyk/RegisterActivity.kt
+++ app/src/main/java/com/kumsal/kyk/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.view.KeyEvent
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -38,26 +39,27 @@ class RegisterActivity : AppCompatActivity() {
 
 
         register.setOnClickListener(View.OnClickListener {
+            println(register())
             if (register()) {
                 mAuth.createUserWithEmailAndPassword(
                     email.text.toString(),
                     password.text.toString()
                 ).addOnSuccessListener {
 
-                    var mMap:HashMap<String,String>
-                    mMap= HashMap()
-                    mMap.set("name_surname",name.text.toString())
-                    mMap.set("image","")
-                    val currId:String=mAuth.uid.toString()
+                    var mMap: HashMap<String, String>
+                    mMap = HashMap()
+                    mMap.set("name_surname", name.text.toString())
+                    mMap.set("image", "")
+                    val currId: String = mAuth.uid.toString()
                     val globals = Globals.ınstance
-                    globals?.uid =currId
-                    mDatabase.child(currId).setValue(mMap).addOnFailureListener {
-                        Exception->
+                    globals?.uid = currId
+                    mDatabase.child(currId).setValue(mMap).addOnFailureListener { Exception ->
                         Toast.makeText(this, Exception.localizedMessage, Toast.LENGTH_LONG).show()
                     }.addOnSuccessListener(
-                        OnSuccessListener <Void>{
+                        OnSuccessListener<Void> {
                             Toast.makeText(this, "Succec", Toast.LENGTH_LONG).show()
-                            val intent:Intent=Intent(applicationContext,MainActivity::class.java)
+                            val intent: Intent =
+                                Intent(applicationContext, MainActivity::class.java)
                             startActivity(intent)
                             this.finish()
                         }
@@ -70,6 +72,13 @@ class RegisterActivity : AppCompatActivity() {
                 }
             }
         })
+
+        password.setOnKeyListener(
+            View.OnKeyListener { v: View?, keyCode: Int, event: KeyEvent? ->
+                true
+
+            }
+        )
     }
 
     private fun register():Boolean {
@@ -80,14 +89,14 @@ class RegisterActivity : AppCompatActivity() {
             name.setError(getString(R.string.mustbe_empy_email))
             troubleCount++
         }
+        if (!isMailValid(email.text.toString())){
+            email.setError(getString(R.string.mail_valid))
+            troubleCount++
+        }
         if (TextUtils.isEmpty(email.text.toString())){
-            if (!isMailValid(email.text.toString())){
-                email.setError(getString(R.string.mail_valid))
-                troubleCount++
-            }else{
                 email.setError(getString(R.string.email_is_empty))
                 troubleCount++
-            }
+
         }
         if (TextUtils.isEmpty(password.text.toString())){
                 if (password.text.toString().length<password.maxLines){
