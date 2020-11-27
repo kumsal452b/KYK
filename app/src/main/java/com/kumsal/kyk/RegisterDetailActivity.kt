a6f4af06a5cfea7cd04aef69fbb0c78464245efa
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-27 22:05:15 +0300

    bınd xml fıle and kotlın fıle

---
+++ app/src/main/java/com/kumsal/kyk/RegisterDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.kumsal.kyk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_detail)
+    }
+}
