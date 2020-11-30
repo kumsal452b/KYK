ad47f649213e36002e7f85386f3c7929c95cd50c
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-11-30 23:24:47 +0300

    problem has found and solved

--- app/src/main/java/com/kumsal/kyk/RegisterDetailActivity.kt
+++ app/src/main/java/com/kumsal/kyk/RegisterDetailActivity.kt
@@ -1,4 +1,4 @@
-package com.kumsal.kyk
+package com.kumsal .kyk
 
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
@@ -23,94 +23,115 @@ import java.util.concurrent.ThreadLocalRandom
 class RegisterDetailActivity : AppCompatActivity() {
 
     private lateinit var username: AutoCompleteTextView
-    private lateinit var imageView:CircleImageView
-    private lateinit var imageBtn:ImageButton
-    private  var advice:Spinner?=null
-    private lateinit var regBtn:Button
-    private lateinit var mAuth:FirebaseAuth
-    private var name:String?=null
-    private var mUsername:DatabaseReference?=null
+    private lateinit var imageView: CircleImageView
+    private lateinit var imageBtn: ImageButton
+    private var advice: Spinner? = null
+    private lateinit var regBtn: Button
+    private lateinit var mAuth: FirebaseAuth
+    private var name: String? = null
+    private var mUsername: DatabaseReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_detail)
-        name=getIntent().getStringExtra("name")
+        name = getIntent().getStringExtra("name")
         println(name)
-        username=findViewById(R.id.register_activity_detail_username);
-        imageView=findViewById(R.id.register_activity_detail_imageView);
-        imageBtn=findViewById(R.id.register_activity_detail_imageButton);
-        advice=findViewById(R.id.register_activity_detail_spinner)
-        regBtn=findViewById(R.id.register_activity_detail_regBtn)
-        mAuth=FirebaseAuth.getInstance()
-        mUsername=FirebaseDatabase.getInstance().getReference("Users")
+        username = findViewById(R.id.register_activity_detail_username);
+        imageView = findViewById(R.id.register_activity_detail_imageView);
+        imageBtn = findViewById(R.id.register_activity_detail_imageButton);
+        advice = findViewById(R.id.register_activity_detail_spinner)
+        regBtn = findViewById(R.id.register_activity_detail_regBtn)
+        mAuth = FirebaseAuth.getInstance()
+        mUsername = FirebaseDatabase.getInstance().getReference("Users")
         generateUsername(name)
 
-        regBtn.setOnClickListener(object: View.OnClickListener{
+        regBtn.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 isEmpty()
             }
         })
     }
 
-    fun isEmpty():Boolean{
-     var troubleCount=0;
-        if (TextUtils.isEmpty(username.text)){
-            if (advice?.selectedItem==null) {
+    fun isEmpty(): Boolean {
+        var troubleCount = 0;
+        if (TextUtils.isEmpty(username.text)) {
+            if (advice?.selectedItem == null) {
                 username.setError(getString(R.string.must_be_leave))
-                MessageDialog.show(this@RegisterDetailActivity,getString(R.string.err),getString(R.string.choose_username),"Okey")
+                MessageDialog.show(
+                    this@RegisterDetailActivity,
+                    getString(R.string.err),
+                    getString(R.string.choose_username),
+                    "Okey"
+                )
                 return false
             }
         }
         return true
     }
 
-    fun generateUsername(ad:String?):List<String>{
-        var result=ArrayList<String>()
-        var username=ArrayList<String>()
-        var name=""
-        var surname=""
-        var fulname=""
-             mUsername?.addValueEventListener(object : ValueEventListener {
-                 override fun onDataChange(snapshot: DataSnapshot) {
-                     for (a in snapshot.children) {
-                         username.add(a.child("username").value.toString())
-                     }
-                     fulname = ad?.trim().toString()
-                     for (a in 0..fulname.length - 1) {
-                         if (fulname.get(a) == ' ') {
-                             if (fulname.get(a + 1) == ' ') {
-                                 continue
-                             } else {
-                                 surname = fulname.substring(a + 1, fulname.length)
-                                 break
-                             }
-                         }
-                         name += fulname.get(a)
-                     }
-                     var ad1=name.trim().toLowerCase()+surname.trim().toLowerCase()
-                     var count=0
-                     if (username.contains(ad1)){
-                         result.add(ad1)
-                         count++
-                     }
-                     var ad2=""
-                     var surnameM=surname.substring(0, 1).toUpperCase() + surname.substring(1)
-                     while (true){
-                         var num=ThreadLocalRandom.current().nextInt(10,100)
-                         if (!username.contains(ad1)){
+    fun generateUsername(ad: String?): List<String> {
+        var result = ArrayList<String>()
+        var username = ArrayList<String>()
+        var name = ""
+        var surname = ""
+        var fulname = ""
+        mUsername?.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (a in snapshot.children) {
+                    username.add(a.child("username").value.toString())
+                }
+                fulname = ad?.trim().toString()
+                for (a in 0..fulname.length - 1) {
+                    if (fulname.get(a) == ' ') {
+                        if (fulname.get(a + 1) == ' ') {
+                            continue
+                        } else {
+                            surname = fulname.substring(a + 1, fulname.length)
+                            break
+                        }
+                    }
+                    name += fulname.get(a)
+                }
+                var ad1 = name.trim().toLowerCase() + surname.trim().toLowerCase()
+                var count = 0
+                if (username.contains(ad1)) {
+                    result.add(ad1)
+                    count++
+                }
+                var ad2 = ""
+                var surnameM = surname.substring(0, 1).toUpperCase() + surname.substring(1)
+                while (true) {
+                    var num = ThreadLocalRandom.current().nextInt(10, 100)
+                    var rnd = Random()
+                    var num2 = rnd.nextInt(1)
 
-                         }
-                     }
+                    if (num2 == 1) {
+                        ad2 = name.trim().toLowerCase() + surnameM + num
+                    }
+                    if (num2 == 0) {
+                        ad2 = name.trim().toLowerCase() + surname + num
+                    }
+                    if (!username.contains(ad2)) {
+                         result.add(ad2)
+                        count++
+                    }
+                    if (count==3){
+                        break
+                    }
+                }
+                var adapter=ArrayAdapter<String>(this@RegisterDetailActivity,android.R.layout.simple_list_item_1,result)
+                advice?.adapter =adapter
 
-                 }
+            }
 
-                 override fun onCancelled(error: DatabaseError) {
-                     println(error)
-                 }
-             })
+            override fun onCancelled(error: DatabaseError) {
+                println(error)
+            }
+        })
 
 
         return result
     }
+
     override fun onBackPressed() {
         super.onBackPressed()
         Animatoo.animateSwipeRight(this)
