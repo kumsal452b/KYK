c7f96ef8145588c4cf68564747b2bcfcc25ef027
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-02 19:34:00 +0300

    new lovcatıon

---
+++ app/src/main/java/com/kumsal/kyk/screns/LoginActivity.kt
@@ -0,0 +1,87 @@
+package com.kumsal.kyk.screns
+
+import android.app.ActivityOptions
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Pair
+import android.view.View
+import android.widget.*
+import com.blogspot.atifsoftwares.animatoolib.Animatoo
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.auth.FirebaseAuth
+import com.kongzue.dialog.v3.TipDialog
+import com.kongzue.dialog.v3.WaitDialog
+import com.kumsal.kyk.MainActivity
+import com.kumsal.kyk.R
+import com.kumsal.kyk.RegisterActivity
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var email:EditText
+    private lateinit var password:EditText
+    private lateinit var login:Button
+    private lateinit var register:Button
+    private lateinit var forgotPsw:TextView
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var imageView:ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        email=findViewById(R.id.login_mail)
+        password=findViewById(R.id.login_password)
+        login=findViewById(R.id.register_activity_detail_regBtn)
+        register=findViewById(R.id.login_register_button)
+        forgotPsw=findViewById(R.id.login_forgot_textView)
+        mAuth=FirebaseAuth.getInstance()
+        imageView=findViewById(R.id.login_imageView)
+
+        login.setOnClickListener(
+            View.OnClickListener {
+                WaitDialog.show(this,"Loading")
+                mAuth.signInWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnFailureListener(
+                    OnFailureListener {
+                        Exception->
+                        TipDialog.show(this, "Problem has found", TipDialog.TYPE.ERROR)
+                        Toast.makeText(applicationContext,Exception.localizedMessage,Toast.LENGTH_LONG).show()
+
+                    }
+                ).addOnCompleteListener(
+                    OnCompleteListener {
+                        task ->
+                        if (task.isSuccessful){
+                            val intent: Intent = Intent(applicationContext, MainActivity::class.java)
+                            var pair:android.util.Pair<View,String>
+
+                            pair= android.util.Pair(imageView,"loginTransImageLogo")
+
+                            var option=ActivityOptions.makeSceneTransitionAnimation(this,pair)
+
+                            startActivity(intent,option.toBundle())
+//                            this.finish()
+                        }else{
+                            Toast.makeText(applicationContext,"error",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                )
+            }
+        )
+        register.setOnClickListener(View.OnClickListener {
+            val intent:Intent=Intent(applicationContext, RegisterActivity::class.java)
+            startActivity(intent)
+        })
+        forgotPsw.setOnClickListener(View.OnClickListener {
+            val intent_forgot_password:Intent=Intent(applicationContext, ForgotPasswordActivity::class.java)
+
+            var option=ActivityOptions.makeSceneTransitionAnimation(this,Pair.create(email,"mailEditText"),
+                Pair.create(register,"button"))
+            startActivity(intent_forgot_password,option.toBundle())
+        })
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        Animatoo.animateSlideDown(this)
+    }
+}
