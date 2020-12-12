6cc98ddd2ee1d07b11681ab203eb4470a87e3fce
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-12 20:49:54 +0300

    problem has found and solved

--- app/src/main/java/com/kumsal/kyk/screns/LoginActivity.kt
+++ app/src/main/java/com/kumsal/kyk/screns/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.app.ActivityOptions
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Pair
 import android.view.View
 import android.widget.*
@@ -18,71 +19,100 @@ import com.kumsal.kyk.R
 import com.kumsal.kyk.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var email:EditText
-    private lateinit var password:EditText
-    private lateinit var login:Button
-    private lateinit var register:Button
-    private lateinit var forgotPsw:TextView
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var login: Button
+    private lateinit var register: Button
+    private lateinit var forgotPsw: TextView
     private lateinit var mAuth: FirebaseAuth
-    private lateinit var imageView:ImageView
+    private lateinit var imageView: ImageView
+    private var troubleCount: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        email=findViewById(R.id.login_mail)
-        password=findViewById(R.id.login_password)
-        login=findViewById(R.id.register_activity_detail_regBtn)
-        register=findViewById(R.id.login_register_button)
-        forgotPsw=findViewById(R.id.login_forgot_textView)
-        mAuth=FirebaseAuth.getInstance()
-        imageView=findViewById(R.id.login_imageView)
+        email = findViewById(R.id.login_mail)
+        password = findViewById(R.id.login_password)
+        login = findViewById(R.id.register_activity_detail_regBtn)
+        register = findViewById(R.id.login_register_button)
+        forgotPsw = findViewById(R.id.login_forgot_textView)
+        mAuth = FirebaseAuth.getInstance()
+        imageView = findViewById(R.id.login_imageView)
 
         login.setOnClickListener(
             View.OnClickListener {
-                WaitDialog.show(this,"Loading")
-                mAuth.signInWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnFailureListener(
-                    OnFailureListener {
-                        Exception->
-                        TipDialog.show(this, "Problem has found", TipDialog.TYPE.ERROR)
-                        Toast.makeText(applicationContext,Exception.localizedMessage,Toast.LENGTH_LONG).show()
-                        throw Exception.fillInStackTrace()
-                    }
-                ).addOnCompleteListener(
-                    OnCompleteListener {
-                        task ->
-                        if (task.isSuccessful){
-                            val intent: Intent = Intent(applicationContext, MainActivity::class.java)
-                            var pair:android.util.Pair<View,String>
+                if (!isMailValid(email.text.toString())){
+                    email.setError(getString(R.string.mail_valid))
+                    return@OnClickListener
+                }
+                if (isLogin()){
+                    WaitDialog.show(this, "Loading")
+                    mAuth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                        .addOnFailureListener(
+                            OnFailureListener { Exception ->
+                                TipDialog.show(this, "Problem has found", TipDialog.TYPE.ERROR)
+                                Toast.makeText(
+                                    applicationContext,
+                                    Exception.localizedMessage,
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                throw Exception.fillInStackTrace()
+                            }
+                        ).addOnCompleteListener(
+                            OnCompleteListener { task ->
+                                if (task.isSuccessful) {
+                                    val intent: Intent =
+                                        Intent(applicationContext, MainActivity::class.java)
+                                    var pair: android.util.Pair<View, String>
 
-                            pair= android.util.Pair(imageView,"loginTransImageLogo")
+                                    pair = android.util.Pair(imageView, "loginTransImageLogo")
 
-                            var option=ActivityOptions.makeSceneTransitionAnimation(this,pair)
+                                    var option =
+                                        ActivityOptions.makeSceneTransitionAnimation(this, pair)
+                                    startActivity(intent, option.toBundle())
+                                    this.finish()
 
-                            startActivity(intent,option.toBundle())
-//                            this.finish()
-                        }else{
-                            Toast.makeText(applicationContext,"error",Toast.LENGTH_LONG).show();
-                        }
-                    }
-                )
+                                } else {
+                                    Toast.makeText(applicationContext, "error", Toast.LENGTH_LONG)
+                                        .show();
+                                }
+                            }
+                        )
+                }
             }
         )
         register.setOnClickListener(View.OnClickListener {
-            val intent:Intent=Intent(applicationContext, RegisterActivity::class.java)
+            val intent: Intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
         })
         forgotPsw.setOnClickListener(View.OnClickListener {
-            val intent_forgot_password:Intent=Intent(applicationContext, ForgotPasswordActivity::class.java)
+            val intent_forgot_password: Intent =
+                Intent(applicationContext, ForgotPasswordActivity::class.java)
 
-            var option=ActivityOptions.makeSceneTransitionAnimation(this,Pair.create(email,"mailEditText"),
-                Pair.create(register,"button"))
-            startActivity(intent_forgot_password,option.toBundle())
+            var option = ActivityOptions.makeSceneTransitionAnimation(
+                this, Pair.create(email, "mailEditText"),
+                Pair.create(register, "button")
+            )
+            startActivity(intent_forgot_password, option.toBundle())
         })
     }
-    private fun 
-    private fun isMailValid(mail:CharSequence):Boolean{
+
+    private fun isLogin(): Boolean {
+        if (TextUtils.isEmpty(email.text.toString())) {
+            email.setError(getString(R.string.mustbe_empy_email))
+            troubleCount++
+        }
+        if (TextUtils.isEmpty(email.text.toString())) {
+            password.setError(getString(R.string.password_cannot_be_emty))
+            troubleCount++
+        }
+        return troubleCount <= 0
+    }
+
+    private fun isMailValid(mail: CharSequence): Boolean {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(mail).matches()
     }
+
     override fun onBackPressed() {
         super.onBackPressed()
         Animatoo.animateSlideDown(this)
