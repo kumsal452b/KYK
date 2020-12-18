632a915ed3c3b62d178c88ab9d0e1fd04cc68bef
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-18 20:43:25 +0300

    delete unused element

--- app/src/main/java/com/kumsal/kyk/MainActivity.kt
+++ app/src/main/java/com/kumsal/kyk/MainActivity.kt
@@ -7,8 +7,6 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.view.animation.OvershootInterpolator
 import android.widget.LinearLayout
 import android.widget.TextView
