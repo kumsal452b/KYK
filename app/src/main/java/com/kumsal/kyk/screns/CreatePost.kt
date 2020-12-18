436e3d6428d8e35b7556ce782dd9ad184a566e40
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-18 20:43:42 +0300

    ınıtıla post actıvıty

---
+++ app/src/main/java/com/kumsal/kyk/screns/CreatePost.kt
@@ -0,0 +1,12 @@
+package com.kumsal.kyk.screns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kumsal.kyk.R
+
+class CreatePost : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+    }
+}
