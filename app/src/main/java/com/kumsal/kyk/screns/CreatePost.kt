f202c423fe415e008d601de5132a8b7d0ea434cc
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-22 22:43:50 +0300

    add new post element

--- app/src/main/java/com/kumsal/kyk/screns/CreatePost.kt
+++ app/src/main/java/com/kumsal/kyk/screns/CreatePost.kt
@@ -2,11 +2,36 @@ package com.kumsal.kyk.screns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageButton
+import com.hendraanggrian.socialview.commons.Hashtag
+import com.hendraanggrian.socialview.commons.Mention
+import com.hendraanggrian.widget.SocialAutoCompleteTextView
 import com.kumsal.kyk.R
+import de.hdodenhof.circleimageview.CircleImageView
+import java.util.ArrayList
 
 class CreatePost : AppCompatActivity() {
+    private lateinit var profile_image:CircleImageView
+    private lateinit var select_image:ImageButton
+    private lateinit var share_button:Button
+    private lateinit var post_text_element:SocialAutoCompleteTextView<Hashtag,Mention>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+        profile_image=findViewById(R.id.activity_create_post_image)
+        select_image=findViewById(R.id.activity_create_post_select_image)
+        share_button=findViewById(R.id.activity_create_post_share)
+        post_text_element=findViewById(R.id.activity_create_post_post_text_element)
+
+        var names=ArrayList<Mention>()
+
+        names.add(Mention("Selam"))
+        names.add(Mention("yahya"))
+        names.add(Mention("mahsin"))
+        names.add(Mention("sdsd"))
+        var adapter=ArrayAdapter<Mention>(this,android.R.layout.simple_list_item_1,names)
+        post_text_element.mentionAdapter=adapter
     }
 }
