7b5f889f82d16862f3dbe2611915dbcb2e72c7c0
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-23 23:20:42 +0300

    add new text element

--- app/src/main/java/com/kumsal/kyk/AdapterModel/post_adapter.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/post_adapter.kt
@@ -2,12 +2,25 @@ package com.kumsal.kyk.AdapterModel
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.kumsal.kyk.R
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
 class post_adapter: RecyclerView.Adapter<post_adapter.postHolder>() {
 
     class postHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        
+        var image:CircleImageView=itemView.findViewById(R.id.post_layout_imageView)
+        var postc:TextView=itemView.findViewById(R.id.post_layout_imageView_postContent)
+        var name:TextView=itemView.findViewById(R.id.post_layout_name)
+        var since:TextView=itemView.findViewById(R.id.post_layout_sinceTime)
+        var expanded:TextView=itemView.findViewById(R.id.post_layout_expanded)
+
+        fun BindElement(model:post_model){
+            Picasso.get().load(model.theImage).placeholder(R.drawable.persontwo).into(image)
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): postHolder {
--- app/src/main/java/com/kumsal/kyk/AdapterModel/post_model.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/post_model.kt
@@ -1,5 +1,22 @@
 package com.kumsal.kyk.AdapterModel
 
-class post_model(ad:String, username:String,postContent:String,imageUri:String) {
+import android.text.TextUtils
 
+class post_model(ad: String, username: String, postContent: String, imageUri: String,since:String) {
+    var thead = ""
+    var theusername = ""
+    var thePost = ""
+    var theImage = ""
+    var theSince = ""
+
+    init {
+        if (TextUtils.isEmpty(imageUri)){
+            this.theImage="empty"
+        }else{
+            this.theImage=imageUri
+        }
+        this.theusername=username
+        this.thePost=postContent
+        this.theSince=since
+    }
 }
