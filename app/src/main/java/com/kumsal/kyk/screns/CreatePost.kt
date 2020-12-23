4a66cb782e84aa5a3c7eb44739ba7118ddd4ed0f
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-23 22:57:56 +0300

    nothing

--- app/src/main/java/com/kumsal/kyk/screns/CreatePost.kt
+++ app/src/main/java/com/kumsal/kyk/screns/CreatePost.kt
@@ -3,6 +3,7 @@ package com.kumsal.kyk.screns
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
+import android.text.TextUtils
 import android.text.TextWatcher
 import android.view.View
 import android.widget.ArrayAdapter
@@ -10,10 +11,14 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ServerValue
+import com.google.firebase.storage.StreamDownloadTask
+import com.google.firebase.storage.UploadTask
 import com.hendraanggrian.socialview.commons.Hashtag
 import com.hendraanggrian.socialview.commons.Mention
 import com.hendraanggrian.widget.SocialAutoCompleteTextView
@@ -24,6 +29,7 @@ import com.kumsal.kyk.MainActivity
 import com.kumsal.kyk.R
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
+import java.security.Timestamp
 import java.util.*
 import kotlin.collections.HashMap
 
@@ -66,29 +72,30 @@ class CreatePost : AppCompatActivity() {
         //initialize intent element
         initialDynamic()
         canBeSent()
-        share_button.setOnClickListener(object : View.OnClickListener{
+        share_button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                var postContent=post_text_element.text.toString()
-                var time=ServerValue.TIMESTAMP
-                var values=HashMap<String,String>()
-                WaitDialog.show(this@CreatePost,getString(R.string.please_wait));
+                var postContent = post_text_element.text.toString()
+                var time = ServerValue.TIMESTAMP
+                var values = HashMap<String, String>()
+                var ukey=mPostRefDb.child(userid).push().key.toString()
+                WaitDialog.show(this@CreatePost, getString(R.string.please_wait));
                 WaitDialog.dismiss(10000)
-                values.put("pc",postContent.toString())
-                values.put("time",time.toString())
-                values.put("name",name)
-                values.put("username",username)
-                mPostRefDb.child(userid).setValue(values).addOnSuccessListener { object :OnSuccessListener<Void>{
-                    override fun onSuccess(p0: Void?) {
-
-                        WaitDialog.dismiss()
-                        var main_Activity= Intent(this@CreatePost,MainActivity::class.java)
-                        startActivity(main_Activity)
-
-                    }
-                } }.addOnFailureListener {
-                    Exception->
+                values.put("pc", postContent.toString())
+                values.put("time", time.toString())
+                values.put("name", name)
+                values.put("username", username)
+                mPostRefDb.child(userid).child(ukey).setValue(values).addOnSuccessListener {
+                            WaitDialog.dismiss()
+                            var main_Activity = Intent(this@CreatePost, MainActivity::class.java)
+                            startActivity(main_Activity)
+                }.addOnFailureListener { Exception ->
                     WaitDialog.dismiss()
-                    Toast.makeText(this@CreatePost,Exception.localizedMessage,Toast.LENGTH_LONG)
+                    Toast.makeText(this@CreatePost, Exception.localizedMessage, Toast.LENGTH_LONG)
+                }.addOnCompleteListener {
+                        WaitDialog.dismiss()
+                        var main_Activity = Intent(this@CreatePost, MainActivity::class.java)
+                        startActivity(main_Activity)
+                        WaitDialog.dismiss()
                 }
             }
         })
@@ -129,6 +136,8 @@ class CreatePost : AppCompatActivity() {
 
         var hint = "What's on your mind, $name?"
         post_text_element.hint = hint
-        Picasso.get().load(imageUri).into(profile_image)
+        if (TextUtils.isEmpty(imageUri)){
+            Picasso.get().load(imageUri).into(profile_image)
+        }
     }
 }
