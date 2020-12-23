5d176aa5e12079b738d8388e82392ea3e1060e36
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-23 22:57:17 +0300

    add new adapter element

---
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/post_adapter.kt
@@ -0,0 +1,24 @@
+package com.kumsal.kyk.AdapterModel
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class post_adapter: RecyclerView.Adapter<post_adapter.postHolder>() {
+
+    class postHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): postHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: postHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+
+    }
+}
