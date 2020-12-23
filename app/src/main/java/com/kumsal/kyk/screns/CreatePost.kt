b753c2036b295cf5cf5dc5b2f49af28f141dd193
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-23 19:14:08 +0300

    add new textwatcher

--- app/src/main/java/com/kumsal/kyk/screns/CreatePost.kt
+++ app/src/main/java/com/kumsal/kyk/screns/CreatePost.kt
@@ -1,6 +1,8 @@
 package com.kumsal.kyk.screns
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageButton
@@ -27,13 +29,7 @@ class CreatePost : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
-        profile_image=findViewById(R.id.activity_create_post_image)
-        select_image=findViewById(R.id.activity_create_post_select_image)
-        share_button=findViewById(R.id.activity_create_post_share)
-        post_text_element=findViewById(R.id.activity_create_post_post_text_element)
-
-        var names=ArrayList<Mention>()
-
+        initialComponent()
         val mention1 = Mention("dirtyhobo")
         val mention2 = Mention.Builder("hobo")
             .setDisplayname("Regular Hobo")
@@ -43,21 +39,54 @@ class CreatePost : AppCompatActivity() {
             .setDisplayname("Hendra Anggrian")
             .setAvatarURL("https://avatars0.githubusercontent.com/u/11507430?v=3&s=460")
             .build()
-        names.add(mention2)
-        names.add(mention1)
-        names.add(mention3)
-        var adapter=ArrayAdapter<Mention>(this, android.R.layout.simple_list_item_1, names)
-        val mentions: MutableList<String> = arrayListOf()
-        mentions.add("@hasankucuk")
-        mentions.add("@yahya")
-        mentions.add("@kumsal")
+//        names.add(mention2)
+//        names.add(mention1)
+//        names.add(mention3)
+//        var adapter=ArrayAdapter<Mention>(this, android.R.layout.simple_list_item_1, names)
+//        val mentions: MutableList<String> = arrayListOf()
+//        mentions.add("@hasankucuk")
+//        mentions.add("@yahya")
+//        mentions.add("@kumsal")
 //        post_text_element.setLinkedMention(mentions)
 
         //initialize intent element
-        name=intent.getStringExtra("name") as String
-        imageUri=intent.getStringExtra("uri") as String
-        var hint= "What's on your mind, $name?"
-        post_text_element.hint=hint
+        initialDynamic()
+        canBeSent()
+    }
+
+    private fun canBeSent() {
+        post_text_element.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (post_text_element.length()>0){
+                    share_button.isEnabled=true
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun initialComponent() {
+        profile_image = findViewById(R.id.activity_create_post_image)
+        select_image = findViewById(R.id.activity_create_post_select_image)
+        share_button = findViewById(R.id.activity_create_post_share)
+        post_text_element = findViewById(R.id.activity_create_post_post_text_element)
+
+        var names=ArrayList<Mention>()
+    }
+
+    private fun initialDynamic() {
+        name = intent.getStringExtra("name") as String
+        imageUri = intent.getStringExtra("uri") as String
+        var hint = "What's on your mind, $name?"
+        post_text_element.hint = hint
         Picasso.get().load(imageUri).into(profile_image)
     }
 }
