aed678be29b68d6f1c562821e909fda406c85049
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-25 20:52:11 +0300

    add thumnail element

--- app/src/main/java/com/kumsal/kyk/AdapterModel/post_model.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/post_model.kt
@@ -2,12 +2,13 @@ package com.kumsal.kyk.AdapterModel
 
 import android.text.TextUtils
 
-class post_model(ad: String, username: String, postContent: String, imageUri: String,since:String) {
+class post_model(ad: String, username: String, postContent: String, imageUri: String,since:String,thmbImg:String) {
     var thead = ""
     var theusername = ""
     var thePost = ""
     var theImage = ""
     var theSince = ""
+    var theThmbImg=""
 
     init {
         if (TextUtils.isEmpty(imageUri)){
@@ -19,5 +20,6 @@ class post_model(ad: String, username: String, postContent: String, imageUri: St
         this.thePost=postContent
         this.theSince=since
         this.thead=ad
+        this.theThmbImg=thmbImg
     }
 }
