c1ff6dce5110c29b5eddf9671cc9307e91a13b02
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2020-12-27 21:37:00 +0300

    Nothing

--- app/src/main/java/com/kumsal/kyk/Globals.kt
+++ app/src/main/java/com/kumsal/kyk/Globals.kt
@@ -1,11 +1,12 @@
 package com.kumsal.kyk
+import java.io.Serializable
 
-import android.graphics.Bitmap
 
-
-class Globals internal constructor() {
+class Globals internal constructor() :Serializable {
     var uid: String? = null
-
+    var name: String? = null
+//    var : String? = null
+//    var uid: String? = null
     companion object {
         private var instance: Globals? = null
         val ınstance: Globals?
