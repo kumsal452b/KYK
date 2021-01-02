6fc78a28596b0cdd9e54fd40d517b1e4d7b74f75
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-02 22:08:52 +0300

    add new field

--- app/src/main/java/com/kumsal/kyk/AdapterModel/security_model.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/security_model.kt
@@ -1,10 +1,12 @@
 package com.kumsal.kyk.AdapterModel
 
-class security_model(name:String, username:String) {
+class security_model(name:String, username:String,image:String) {
     var thename=""
     var theusername=""
+    var theimage=""
     init {
         this.thename=name
         this.theusername=username
+        this.theimage=image
     }
 }
