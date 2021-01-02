720f8badfc7d21c916058e92130a6488d38886f7
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-02 20:39:48 +0300

    add new adapter class for secure element

---
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/security_adapter.kt
@@ -0,0 +1,10 @@
+package com.kumsal.kyk.AdapterModel
+
+import androidx.recyclerview.widget.RecyclerView
+import com.mikiloz.fancyadapters.SelectableAdapter
+
+class security_adapter:SelectableAdapter<> {
+
+    class secureHolder:RecyclerView
+
+}
