15b4b9ee13ad15ebe96a9c1f71f0c20c31a34085
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-02 20:40:33 +0300

    add new model element

---
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/security_model.kt
@@ -0,0 +1,10 @@
+package com.kumsal.kyk.AdapterModel
+
+class security_model(name:String, username:String) {
+    var thename=""
+    var theusername=""
+    init {
+        this.thename=name
+        this.theusername=username
+    }
+}
