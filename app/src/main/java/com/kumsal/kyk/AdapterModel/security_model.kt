87438451d5b804926bddda790371b8ec90b97abd
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-02 23:20:03 +0300

    problem has fixed

--- app/src/main/java/com/kumsal/kyk/AdapterModel/security_model.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/security_model.kt
@@ -1,12 +1,18 @@
 package com.kumsal.kyk.AdapterModel
 
+import android.text.TextUtils
+
 class security_model(name:String, username:String,image:String) {
     var thename=""
     var theusername=""
     var theimage=""
     init {
+        if (TextUtils.isEmpty(image) || image==null){
+            this.theimage="empty"
+        }else{
+            this.theimage=image
+        }
         this.thename=name
         this.theusername=username
-        this.theimage=image
     }
 }
