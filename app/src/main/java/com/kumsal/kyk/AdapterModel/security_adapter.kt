b161cd2d2ccd7158f8616ec98e73609e230c93d6
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-03 23:58:18 +0300

    testeet is succecfuly

--- app/src/main/java/com/kumsal/kyk/AdapterModel/security_adapter.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/security_adapter.kt
@@ -1,10 +1,13 @@
 package com.kumsal.kyk.AdapterModel
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.view.ActionMode
+import androidx.appcompat.view.SupportActionModeWrapper
 import androidx.recyclerview.widget.RecyclerView
 import com.kumsal.kyk.R
 import com.mikiloz.fancyadapters.SelectableAdapter
@@ -13,7 +16,7 @@ import de.hdodenhof.circleimageview.CircleImageView
 import org.w3c.dom.Text
 import java.util.ArrayList
 
-class security_adapter(items:ArrayList<security_model>): SelectableAdapter<security_model, security_adapter.secureHolder>(items) {
+class security_adapter(items:ArrayList<security_model>,context: Context): SelectableAdapter<security_model, security_adapter.secureHolder>(items) {
 
     class secureHolder(itemView:View):RecyclerView.ViewHolder(itemView){
         var imageUrl:CircleImageView
@@ -24,7 +27,14 @@ class security_adapter(items:ArrayList<security_model>): SelectableAdapter<secur
             imageUrl=itemView.findViewById(R.id.secure_image)
             name=itemView.findViewById(R.id.secure_name)
             username=itemView.findViewById(R.id.secure_username)
+            itemView.setOnLongClickListener(object:View.OnLongClickListener{
+                override fun onLongClick(v: View?): Boolean {
+                    if ()
+                    return true
+                }
+            })
         }
+
         fun bindElement(theModel:security_model){
             Picasso.get().load(theModel.theimage).into(imageUrl)
             name.setText(theModel.thename)
@@ -50,8 +60,13 @@ class security_adapter(items:ArrayList<security_model>): SelectableAdapter<secur
         TODO("Not yet implemented")
     }
 
+
     override fun startActionMode(): ActionMode {
-        TODO("Not yet implemented")
+        var callback:ActionMode.Callback
+        callback=SupportActionModeWrapper.CallbackWrapper(context,)
+        var action:ActionMode
+        action=ActionMode()
+        return AppCompatDelegate.
     }
 
     override fun onSelectionUpdate(p0: ActionMode?, p1: Int) {
