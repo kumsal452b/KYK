3ebbe34a394c9207219d37bb27e8651d222f6f23
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-19 17:47:25 +0300

    some unused code element has deleted

--- app/src/main/java/com/kumsal/kyk/AdapterModel/security_adapter.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/security_adapter.kt
@@ -15,31 +15,24 @@ import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import java.util.*
 
-open class security_adapter(items: ArrayList<security_model>, private val context: Context?, recyclerView: RecyclerView?): SuperSelectableAdapter<security_model, security_adapter.secureHolder>(items,recyclerView,ItemTouchHelper.UP,ItemTouchHelper.ANIMATION_TYPE_SWIPE_SUCCESS) {
-    companion object{
-        var secureAdapter:security_adapter=security_adapter()
+class security_adapter(private var items: ArrayList<security_model>, private val context: Context?):
+    RecyclerView.Adapter<security_adapter.secureHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): secureHolder {
+        var view=LayoutInflater.from(parent?.context).inflate(R.layout.secure_single, parent, false)
+        return secureHolder(view)
     }
 
-    constructor() : this(ArrayList(),null,null)
-    init {
-        isDragging
+    override fun getItemCount(): Int {
+        return items.size
     }
+
     inner class secureHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
         var imageUrl:CircleImageView = itemView.findViewById(R.id.secure_image)
         var name:TextView = itemView.findViewById(R.id.secure_name)
         var username:TextView = itemView.findViewById(R.id.secure_username)
         var overlay:View=itemView.findViewById(R.id.overLay)
         init {
-            itemView.setOnLongClickListener(object : View.OnLongClickListener {
-                override fun onLongClick(v: View?): Boolean {
-                    if (!isActionModeEnabled){
-                        //startDrag(this@secureHolder,layoutPosition)
-                        return true
-                    }else{
-                        return false
-                    }
-                }
-            })
+            
         }
 
         fun bindElement(theModel: security_model){
@@ -50,95 +43,8 @@ open class security_adapter(items: ArrayList<security_model>, private val contex
     }
 
     override fun onBindViewHolder(p0: secureHolder, p1: Int) {
-        if (isActionModeEnabled){
-            p0.overlay.setVisibility(View.VISIBLE);
-        }
-        else{
-            p0.overlay.setVisibility(View.GONE)
-        }
-        if (isActionModeEnabled){
-            p0.overlay.setBackgroundColor(
-                ContextCompat.getColor(context as Context, R.color.black));
-        }
-        else{
-            p0.overlay.setBackgroundColor(
-                ContextCompat.getColor(context as Context, R.color.white));
-        }
         var theModel=items.get(p1)
         p0.bindElement(theModel)
     }
 
-    override fun onCreateSelectableViewHolder(parent: ViewGroup?, viewType: Int): secureHolder {
-        var view=LayoutInflater.from(parent?.context).inflate(R.layout.secure_single, parent, false)
-        return secureHolder(view)
-    }
-
-    override fun onItemSelected(p0: secureHolder?, p1: Int) {
-        p0?.overlay?.setBackgroundColor(Color.GRAY)
-    }
-
-    override fun onItemDeselected(p0: secureHolder?, p1: Int) {
-        TODO("Not yet implemented")
-    }
-
-
-    override fun startActionMode(): ActionMode {
-
-        var appCompatActivity=AppCompatActivity()
-
-
-
-        return  appCompatActivity.startSupportActionMode(object : AdapterActionModeCallback() {
-            override fun onCreateActionMode(p0: ActionMode?, p1: Menu?): Boolean {
-                p0?.menuInflater?.inflate(R.menu.secure_menu, p1)
-
-
-                for (holder in viewHolders) {
-
-                    if (holder != null) {
-                        holder.overlay.visibility = View.VISIBLE
-                        var position = holder.layoutPosition
-                        if (position != -1 && isSelected(position)) {
-                            holder.overlay.setBackgroundColor(
-                                ContextCompat.getColor(
-                                    context as Context, R.color.blackAlpha30
-                                )
-                            )
-                        } else {
-                            holder.overlay.setBackgroundColor(
-                                ContextCompat.getColor(
-                                    context as Context, R.color.colorPrimary
-                                )
-                            )
-                        }
-                    }
-                }
-                return true
-            }
-
-            override fun onPrepareActionMode(p0: ActionMode?, p1: Menu?): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun onActionItemClicked(p0: ActionMode?, p1: MenuItem?): Boolean {
-                TODO("Not yet implemented")
-            }
-
-            override fun onExitActionMode(p0: ActionMode?) {
-                TODO("Not yet implemented")
-            }
-        }) as ActionMode
-    }
-
-    override fun onSelectionUpdate(p0: ActionMode?, p1: Int) {
-        p0?.title=p1.toString() + " item(s)"
-    }
-
-    override fun onMove(p0: secureHolder?, p1: secureHolder?) {
-        TODO("Not yet implemented")
-    }
-
-    override fun onSwipe(p0: secureHolder?, p1: Int) {
-        TODO("Not yet implemented")
-    }
 }
