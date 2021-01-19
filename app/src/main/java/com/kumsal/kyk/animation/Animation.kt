1bc8704719131f573126c7eab303b161bde7fb54
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-01-19 18:14:15 +0300

    add apply transform element

--- app/src/main/java/com/kumsal/kyk/animation/Animation.kt
+++ app/src/main/java/com/kumsal/kyk/animation/Animation.kt
@@ -1,7 +1,24 @@
 package com.kumsal.kyk.animation
 
+import android.view.View
 import android.view.animation.Animation
+import android.view.animation.Transformation
 
-open class Animation: Animation() {
-    
+open class Animation(width:Int, view: View): Animation() {
+    private var width=0
+    private var startWidth=0;
+    private var view:View
+    init {
+        this.view=view
+        this.width=width
+        this.startWidth=view.width
+    }
+
+    override fun applyTransformation(interpolatedTime: Float, t: Transformation?) {
+        var newWidth=startWidth+((width-startWidth)*interpolatedTime) as Int
+        view.layoutParams.width=newWidth
+        view.requestLayout()
+        
+    }
+
 }
