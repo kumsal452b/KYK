321972d80895b15a48b9f4e712070417163f02fe
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-01 22:55:02 +0300

    model class modification have done

--- app/src/main/java/com/kumsal/kyk/AdapterModel/UsersModel.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/UsersModel.kt
@@ -11,11 +11,29 @@ class UsersModel {
     var theThmbImage: String? = null
     var theTime: Timestamp? = null
     var theImage: String? = null
+    var theId:String?=null
 
     constructor() {
 
     }
 
+    constructor(
+        theEmail: String?,
+        theNameSurname: String?,
+        theUserName: String?,
+        theThmbImage: String?,
+        theTime: Timestamp?,
+        theImage: String?,
+        theId:String?
+    ) {
+        this.theEmail = theEmail
+        this.theNameSurname = theNameSurname
+        this.theUserName = theUserName
+        this.theThmbImage = theThmbImage
+        this.theTime = theTime
+        this.theImage = theImage
+        this.theId=theId
+    }
     constructor(
         theEmail: String?,
         theNameSurname: String?,
@@ -34,12 +52,13 @@ class UsersModel {
 
     fun toMap(): Map<String, Any> {
         val result = HashMap<String, Any>()
-        result.put("email", theEmail!!)
-        result.put("name", theNameSurname!!)
-        result.put("image", theImage!!)
-        result.put("thmbImage", theThmbImage!!)
-        result.put("time", theTime!!)
-        result.put("username", theUserName!!)
+        result.put("theEmail", theEmail!!)
+        result.put("theNameSurname", theNameSurname!!)
+        result.put("theImage", theImage!!)
+        result.put("theThmbImage", theThmbImage!!)
+        result.put("theTime", theTime!!)
+        result.put("theUserName", theUserName!!)
+        result.put("theId",theId!!)
         return result
     }
 
