418e8c8ea32342a3d7c70b4ff0452aa2c055837b
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-03 00:19:47 +0300

    problemhave been not solved

--- app/src/main/java/com/kumsal/kyk/DBModels/DbUsers.kt
+++ app/src/main/java/com/kumsal/kyk/DBModels/DbUsers.kt
@@ -1,20 +1,38 @@
 package com.kumsal.kyk.DBModels
 
+import android.text.TextUtils
+import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
 import com.kumsal.kyk.interfaces.Users
+import java.lang.reflect.Method
 import java.util.ArrayList
 
 class DbUsers<Model> : Users<Model>{
     var mDbFirestore:FirebaseFirestore?=null
     var theModel:Model?=null
     var theModelList:ArrayList<Model>?=null
+    private var className=""
 
-    constructor(mDbFirestore: FirebaseFirestore?) {
+    constructor(mDbFirestore: FirebaseFirestore?,className:String) {
         this.mDbFirestore = mDbFirestore
+        this.className=className
     }
 
-    fun getElement(element: Users<Model>,collectionName:String){
-        mDbFirestore.collection()
+    fun getElement(element: Users<Model>,collectionName:String,docName:String):Model{
+        if (TextUtils.isEmpty(docName)){
+            mDbFirestore.collection(collectionName).addSnapshotListener { doc, e ->
+                if (e!=null){
+                    Log.d("Error", e.message as String)
+                    return@addSnapshotListener
+                }
+                
+                var theModel:Class<Model>=
+                for (theDoc in doc!!){
+                    var theData=theDoc.toObject(Class<Model>::class.java)
+                }
+
+            }
+        }
     }
 
     override fun getUsers(array: ArrayList<Model>) {
