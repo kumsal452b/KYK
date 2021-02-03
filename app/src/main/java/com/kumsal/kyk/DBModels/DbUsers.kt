cee57cf0763b29392256b213f1a254deb3d89538
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-03 21:01:20 +0300

    some codee element haschanged

--- app/src/main/java/com/kumsal/kyk/DBModels/DbUsers.kt
+++ app/src/main/java/com/kumsal/kyk/DBModels/DbUsers.kt
@@ -3,8 +3,11 @@ package com.kumsal.kyk.DBModels
 import android.text.TextUtils
 import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.gson.reflect.TypeToken
+import com.kumsal.kyk.interfaces.UserListCallback
 import com.kumsal.kyk.interfaces.Users
 import java.lang.reflect.Method
+import java.lang.reflect.ParameterizedType
 import java.util.ArrayList
 
 class DbUsers<Model> : Users<Model>{
@@ -12,27 +15,28 @@ class DbUsers<Model> : Users<Model>{
     var theModel:Model?=null
     var theModelList:ArrayList<Model>?=null
     private var className=""
-
-    constructor(mDbFirestore: FirebaseFirestore?,className:String) {
+    constructor(mDbFirestore: FirebaseFirestore?) {
         this.mDbFirestore = mDbFirestore
-        this.className=className
     }
 
-    fun getElement(element: Users<Model>,collectionName:String,docName:String):Model{
+    fun readyElement(element: Users<Model>,collectionName:String,docName:String):ArrayList<Model>?{
         if (TextUtils.isEmpty(docName)){
-            mDbFirestore.collection(collectionName).addSnapshotListener { doc, e ->
+            mDbFirestore?.collection(collectionName)?.addSnapshotListener { doc, e ->
                 if (e!=null){
                     Log.d("Error", e.message as String)
                     return@addSnapshotListener
                 }
-                
-                var theModel:Class<Model>=
                 for (theDoc in doc!!){
-                    var theData=theDoc.toObject(Class<Model>::class.java)
+                    var theData=theDoc.toObject(javaClass)
+//                    theModelList.add(theData)
                 }
 
             }
         }
+        return theModelList
+    }
+    fun getElement(){
+        readyElement(this,"Users","")
     }
 
     override fun getUsers(array: ArrayList<Model>) {
