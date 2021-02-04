db2af947f7527e19c00bc1a6f13e99f4e54d8910
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-04 17:10:38 +0300

    some element have private

--- app/src/main/java/com/kumsal/kyk/DBModels/DbUsers.kt
+++ app/src/main/java/com/kumsal/kyk/DBModels/DbUsers.kt
@@ -2,52 +2,53 @@ package com.kumsal.kyk.DBModels
 
 import android.text.TextUtils
 import android.util.Log
-import android.view.Display
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.gson.reflect.TypeToken
-import com.kumsal.kyk.interfaces.UserListCallback
-import com.kumsal.kyk.interfaces.Users
-import java.lang.reflect.Method
-import java.lang.reflect.ParameterizedType
+import com.kumsal.kyk.interfaces.GetCenter
 import java.util.ArrayList
 
-class DbUsers<Model> : Users<Model>{
-    var mDbFirestore:FirebaseFirestore?=null
-    var theModel:Model?=null
-    var theModelList:ArrayList<Model>?=null
-    var className:Class<Model>?=null
-     private var model:Model
+class DbUsers<Model> {
+    private var mDbFirestore: FirebaseFirestore? = null
+    private var theModel: Model? = null
+    private var theModelList: ArrayList<Model>? = null
+    private var className: Class<Model>? = null
+    private var model: Model
 
 
-    constructor(mDbFirestore: FirebaseFirestore?,gClass:Model) {
+    constructor(mDbFirestore: FirebaseFirestore?, gClass: Model) {
         this.mDbFirestore = mDbFirestore
-        this.model=gClass
+        this.model = gClass
     }
-    fun getModel():Model{
+
+    fun getModel(): Model {
         return model
     }
 
-    fun readyElement(element: Users<Model>,collectionName:String,docName:String):ArrayList<Model>?{
-        if (TextUtils.isEmpty(docName)){
+    private fun readyElement(
+        element: GetCenter<Model>,
+        collectionName: String,
+        docName: String
+    ) {
+        if (TextUtils.isEmpty(docName)) {
             mDbFirestore?.collection(collectionName)?.addSnapshotListener { doc, e ->
-                if (e!=null){
-                    Log.d("Error", e.message as String)
+                if (e != null) {
+                    Log.d("Error in Dbusers", e.message as String)
                     return@addSnapshotListener
                 }
-                for (theDoc in doc!!){
-                    var theData=theDoc.toObject(model!!::class.java)
+                for (theDoc in doc!!) {
+                    var theData = theDoc.toObject(model!!::class.java)
                     theModelList?.add(theData)
                 }
-
+                element.getUsers(theModelList!!)
             }
         }
-        return theModelList
-    }
-    fun getElement(){
-        readyElement(this,"Users","")
     }
 
-    override fun getUsers(array: ArrayList<Model>) {
-
+    fun getElement(collectionName:String,docName: String?) :ArrayList<Model>{
+        readyElement(object:GetCenter<Model>{
+            override fun getUsers(array: ArrayList<Model>) {
+                theModelList=array
+            }
+        }, collectionName, docName!!)
+        return theModelList!!
     }
 }
