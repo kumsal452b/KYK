152f2fe291a2ba898299491b77e1cef9d0eaf35b
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-10 23:56:12 +0300

    nothing

--- app/src/main/java/com/kumsal/kyk/AdapterModel/security_model.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/security_model.kt
@@ -1,6 +1,7 @@
 package com.kumsal.kyk.AdapterModel
 
 import android.text.TextUtils
+import com.google.gson.JsonObject
 
 class security_model{
     var thename:String?=null
@@ -8,14 +9,17 @@ class security_model{
     var theimage:String?=null
     var theisChecked:Boolean?=null
     var thePersonId:String?=null
+    var keys11:JsonObject?=null
     constructor(){}
     constructor(
+
         thename: String,
         theusername: String,
         theimage: String,
         theisChecked: Boolean,
         thePersonId: String
     ) {
+
         if (TextUtils.isEmpty(theimage) || theimage==null){
             this.theimage = "empty"
         }else{
