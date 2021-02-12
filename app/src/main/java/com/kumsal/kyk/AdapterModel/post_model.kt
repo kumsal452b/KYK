4cdab94cd64cd9c2c6e5f3b261a793f6df610fb7
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-12 19:49:24 +0300

    model class have been hanged

--- app/src/main/java/com/kumsal/kyk/AdapterModel/post_model.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/post_model.kt
@@ -2,6 +2,7 @@ package com.kumsal.kyk.AdapterModel
 
 import android.text.TextUtils
 import com.google.firebase.Timestamp
+import java.util.ArrayList
 
 class post_model{
     var thead :String?=null
@@ -10,5 +11,26 @@ class post_model{
     var theImage :String?=null
     var theSince :Timestamp?=null
     var theThmbImg :String?=null
-    
+    var comments:ArrayList<HashMap<String,Any>>?=null
+    var likes:ArrayList<String>?=null
+    constructor(){}
+    constructor(
+        thead: String?,
+        theusername: String?,
+        thePost: String?,
+        theImage: String?,
+        theSince: Timestamp?,
+        theThmbImg: String?,
+        comments: ArrayList<HashMap<String, Any>>?,
+        likes: ArrayList<String>?
+    ) {
+        this.thead = thead
+        this.theusername = theusername
+        this.thePost = thePost
+        this.theImage = theImage
+        this.theSince = theSince
+        this.theThmbImg = theThmbImg
+        this.comments = comments
+        this.likes = likes
+    }
 }
