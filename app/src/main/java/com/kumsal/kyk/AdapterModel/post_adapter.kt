309ed2a20e21d9d35075d07b64b99feb0a8815f3
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-16 12:39:59 +0300

    problem have been not foundt yet

--- app/src/main/java/com/kumsal/kyk/AdapterModel/post_adapter.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/post_adapter.kt
@@ -1,6 +1,7 @@
 package com.kumsal.kyk.AdapterModel
 
 import android.content.Context
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,32 +15,34 @@ import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import java.util.ArrayList
 
-class post_adapter(private var list:ArrayList<post_model>,private var context:Context): RecyclerView.Adapter<post_adapter.postHolder>() {
+class post_adapter(private var list: ArrayList<post_model>, private var context: Context) :
+    RecyclerView.Adapter<post_adapter.postHolder>() {
 
     class postHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var image:CircleImageView=itemView.findViewById(R.id.post_layout_imageView)
-        var postc:SocialTextView=itemView.findViewById(R.id.post_layout_imageView_postContent)
-        var name:TextView=itemView.findViewById(R.id.post_layout_name)
-        var username:TextView=itemView.findViewById(R.id.post_layout_username)
-        var since:TextView=itemView.findViewById(R.id.post_layout_sinceTime)
-        var expanded:ImageButton=itemView.findViewById(R.id.post_layout_expanded)
+        var image: CircleImageView = itemView.findViewById(R.id.post_layout_imageView)
+        var postc: SocialTextView = itemView.findViewById(R.id.post_layout_imageView_postContent)
+        var name: TextView = itemView.findViewById(R.id.post_layout_name)
+        var username: TextView = itemView.findViewById(R.id.post_layout_username)
+        var since: TextView = itemView.findViewById(R.id.post_layout_sinceTime)
+        var expanded: ImageButton = itemView.findViewById(R.id.post_layout_expanded)
 
-        fun BindElement(model:post_model){
-            Picasso.get().load(model.thmbImageUri).placeholder(R.drawable.persontwo).into(image)
+        fun BindElement(model: post_model) {
+            if (!TextUtils.isEmpty(model.thmbImageUri))
+                Picasso.get().load(model.thmbImageUri).placeholder(R.drawable.persontwo).into(image)
             postc.setText(model.pc)
             name.setText(model.name)
-            since.setText(model.time!!.nanoseconds)
+//            since.setText(model.time!!.nanoseconds)
             username.setText(model.username)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): postHolder {
-        var view=LayoutInflater.from(context).inflate(R.layout.post_layout,parent,false)
+        var view = LayoutInflater.from(context).inflate(R.layout.post_layout, parent, false)
         return postHolder(view)
     }
 
     override fun onBindViewHolder(holder: postHolder, position: Int) {
-        var theModel=list.get(position)
+        var theModel = list.get(position)
         holder.BindElement(theModel)
     }
 
