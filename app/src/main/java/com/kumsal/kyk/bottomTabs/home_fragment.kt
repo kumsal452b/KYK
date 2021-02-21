08246b92b20b5b61e10f33dfb9df2f948fc7d6b3
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-21 16:18:20 +0300

    add new orderby unıt

--- app/src/main/java/com/kumsal/kyk/bottomTabs/home_fragment.kt
+++ app/src/main/java/com/kumsal/kyk/bottomTabs/home_fragment.kt
@@ -13,6 +13,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.google.firebase.firestore.FirebaseFirestore
+import com.kumsal.kyk.AdapterModel.UsersModel
 import com.kumsal.kyk.AdapterModel.post_adapter
 import com.kumsal.kyk.AdapterModel.post_model
 import com.kumsal.kyk.R
@@ -21,88 +22,101 @@ import java.util.*
 import kotlin.collections.ArrayList
 
 
-class home_fragment : Fragment(){
+class home_fragment : Fragment() {
 
     private var mUser: FirebaseUser? = null
-    private var mPostDb: DatabaseReference?=null
-    private var mFsPostDb:FirebaseFirestore?=null
-    private var mFsAuthDb:FirebaseFirestore?=null
+    private var mPostDb: DatabaseReference? = null
+    private var mFsPostDb: FirebaseFirestore? = null
+    private var mFsAuthDb: FirebaseFirestore? = null
+
     //Adapter section
     lateinit var adapter: post_adapter
     lateinit var post_list: ArrayList<post_model>
-    private lateinit var query:Query
+    private lateinit var query: Query
+
     //reccler
     private lateinit var recyclerView: RecyclerView
-//
+
+    //
 //    lateinit var adapter11: FirebaseRecyclerAdapter<post_model, Post>
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        var view=inflater.inflate(R.layout.fragment_home, container, false)
+        var view = inflater.inflate(R.layout.fragment_home, container, false)
         //Adapter initial
-        post_list= ArrayList()
-        adapter= post_adapter(post_list, view.context)
+        post_list = ArrayList()
+        adapter = post_adapter(post_list, view.context)
 
         //Recycler initialze
-        recyclerView=view.findViewById(R.id.fragment_home_recycler)
+        recyclerView = view.findViewById(R.id.fragment_home_recycler)
         recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager= LinearLayoutManager(view.context)
-        recyclerView.adapter=adapter
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+        recyclerView.adapter = adapter
 
         //
         mPostDb = FirebaseDatabase.getInstance().getReference("Post")
-        mFsPostDb= FirebaseFirestore.getInstance()
-        mFsAuthDb= FirebaseFirestore.getInstance()
-        mUser=FirebaseAuth.getInstance().currentUser
-        if (mUser!=null){
+        mFsPostDb = FirebaseFirestore.getInstance()
+        mFsAuthDb = FirebaseFirestore.getInstance()
+        mUser = FirebaseAuth.getInstance().currentUser
+        if (mUser != null) {
             getPostValue()
 //            recyclerView.adapter=adapter11
         }
         return view
     }
-    var getUserName=ArrayList<String>()
-    fun getDeniedPerson(theGetElement:GetCenter<String>){
-        mFsAuthDb?.collection("Users")?.document(mUser?.uid!!)?.get()?.addOnSuccessListener{
-            documents->
-            var blockerList=documents["blockers"]
-            var blockedList=documents["blocked"]
 
-            if (blockerList==null)
-                blockerList=ArrayList<String>()
-            if (blockedList==null)
-                blockedList=ArrayList<String>()
-            theGetElement.getUsers(blockedList as ArrayList<String>,blockerList as ArrayList<String>)
+    var getUserName = ArrayList<String>()
+    fun getDeniedPerson(theGetElement: GetCenter<String>) {
+        mFsAuthDb?.collection("Users")?.document(mUser?.uid!!)?.get()
+            ?.addOnSuccessListener { documents ->
+                var blockerList = documents["blockers"]
+                var blockedList = documents["blocked"]
 
-        }?.addOnFailureListener{
-            Log.d("Home fragment",it.message!!)
+                if (blockerList == null)
+                    blockerList = ArrayList<String>()
+                if (blockedList == null)
+                    blockedList = ArrayList<String>()
+                theGetElement.getUsers(
+                    blockedList as ArrayList<String>,
+                    blockerList as ArrayList<String>
+                )
+
+            }?.addOnFailureListener {
+            Log.d("Home fragment", it.message!!)
         }
     }
-    fun getPostValue() {
-        mFsPostDb?.collection("Post")?.orderBy("theTime")?.get()?.addOnFailureListener(
-            OnFailureListener {
-                Log.d("Home fragment",it.message!!)
-            }
-        )?.addOnSuccessListener{document->
-            
-            getDeniedPerson(object : GetCenter<String>{
-                override fun getUsers(
-                    blocked: java.util.ArrayList<String>,
-                    blocker: java.util.ArrayList<String>
-                ) {
-                    for (doc in document) {
 
-                        var thePost = doc.toObject(post_model::class.java)
-                        if (!blocked.contains(thePost.username) && !blocker.contains(thePost.username)) {
-                            post_list.add(thePost)
-                        }
-                    }
-                    adapter.notifyDataSetChanged()
+    fun getPostValue() {
+        var collectionReference = mFsPostDb?.collection("Post")
+        var query = collectionReference?.orderBy(
+            "theTime",
+            com.google.firebase.firestore.Query.Direction.ASCENDING
+        )
+
+           query?.get()?.addOnFailureListener(
+                OnFailureListener {
+                    Log.d("Home fragment", it.message!!)
                 }
-            })
-        }
-        query = mPostDb as Query
+            )?.addOnSuccessListener { document ->
+                getDeniedPerson(object : GetCenter<String> {
+                    override fun getUsers(
+                        blocked: java.util.ArrayList<String>,
+                        blocker: java.util.ArrayList<String>
+                    ) {
+                        for (doc in document) {
+
+                            var thePost = doc.toObject(post_model::class.java)
+                            if (!blocked.contains(thePost.username) && !blocker.contains(thePost.username)) {
+                                post_list.add(thePost)
+                            }
+                        }
+                        adapter.notifyDataSetChanged()
+                    }
+                })
+            }
+//        query = mPostDb as Query
 //
-   }
+    }
 }
