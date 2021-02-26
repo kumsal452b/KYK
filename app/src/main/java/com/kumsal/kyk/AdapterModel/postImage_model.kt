e9041817db1f8b6e7f4c2b57d566ce0ae7b749a2
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-26 23:56:30 +0300

    delete access modifer

--- app/src/main/java/com/kumsal/kyk/AdapterModel/postImage_model.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/postImage_model.kt
@@ -3,7 +3,7 @@ package com.kumsal.kyk.AdapterModel
 import android.net.Uri
 
 class postImage_model{
-    private var imageUri:Uri?=null
+     var imageUri:Uri?=null
 
     constructor()
     constructor(imageUri: Uri?) {
