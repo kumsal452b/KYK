d78ee9f68cb207cb37f83a62846f1933dff8e5d8
kumsal65 <yahya.alatas.mdbf17@iste.edu.tr>
2021-02-27 00:03:59 +0300

    mission have done

--- app/src/main/java/com/kumsal/kyk/AdapterModel/postImage_adapter.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/postImage_adapter.kt
@@ -8,8 +8,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kumsal.kyk.R
 import com.squareup.picasso.Picasso
 
-class postImage_adapter :RecyclerView.Adapter<post_adapter.postHolder>{
+class postImage_adapter :RecyclerView.Adapter<postImage_adapter.postImageHolder>{
     private var imageList:ArrayList<postImage_model>?=null
+    constructor()
+    constructor(imageList: ArrayList<postImage_model>?) : super() {
+        this.imageList = imageList
+    }
 
     inner class postImageHolder(itemView:View) : RecyclerView.ViewHolder(itemView){
         var imageView:ImageView=itemView.findViewById(R.id.post_single_image)
@@ -18,15 +22,16 @@ class postImage_adapter :RecyclerView.Adapter<post_adapter.postHolder>{
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): post_adapter.postHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): postImage_adapter.postImageHolder {
         var view=LayoutInflater.from(parent.context).inflate(R.layout.post_single_image,parent,false)
+        return postImageHolder(view)
     }
 
-    override fun onBindViewHolder(holder: post_adapter.postHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: postImage_adapter.postImageHolder, position: Int) {
+        holder.bindElement(imageList?.get(position)!!)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return imageList?.size!!
     }
 }
