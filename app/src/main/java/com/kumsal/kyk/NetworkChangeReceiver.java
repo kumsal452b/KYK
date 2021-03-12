e608b99515ad4ac7af7c56529ef54a60c271a81f
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-03-12 21:23:13 +0300

    add new connection element

---
+++ app/src/main/java/com/kumsal/kyk/NetworkChangeReceiver.java
@@ -0,0 +1,12 @@
+package com.kumsal.kyk;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        System.out.println("connection change");
+    }
+}
