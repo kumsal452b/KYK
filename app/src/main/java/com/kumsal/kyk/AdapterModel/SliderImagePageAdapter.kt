d42e075ade8da7b7ef66fc51b0131cc3707175fa
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-04-10 22:27:47 +0300

    pager adapter have desıgned

--- app/src/main/java/com/kumsal/kyk/AdapterModel/SliderImagePageAdapter.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/SliderImagePageAdapter.kt
@@ -2,23 +2,35 @@ package com.kumsal.kyk.AdapterModel
 
 import android.content.Context
 import android.net.Uri
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.viewpager.widget.PagerAdapter
+import com.kumsal.kyk.R
 
-class SliderImagePageAdapter : PagerAdapter() {
+class SliderImagePageAdapter : PagerAdapter {
     var context:Context?=null
     var uriList:ArrayList<Uri>?=null
+    var mlayoutInflater:LayoutInflater?=null
     constructor(context: Context?, uriList: ArrayList<Uri>?) : super() {
         this.context = context
         this.uriList = uriList
+        mlayoutInflater=context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     }
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return uriList?.size!!
     }
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
-        TODO("Not yet implemented")
+            return view==`object` as LinearLayout
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        var itemView=mlayoutInflater?.inflate(R.layout.slider_image_for_post,container,false) as View
+
+        return super.instantiateItem(container, position)
     }
 }
