559d32c0fa4b30ea61e4aaf3b2a347304216c648
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-04-21 23:58:41 +0300

    add new parameter

--- app/src/main/java/com/kumsal/kyk/interfaces/GetCenter.kt
+++ app/src/main/java/com/kumsal/kyk/interfaces/GetCenter.kt
@@ -3,5 +3,5 @@ package com.kumsal.kyk.interfaces
 import java.util.ArrayList
 
 interface GetCenter<Model> {
-    fun getUsers(blocked: ArrayList<Model>,blocker:ArrayList<Model>)
+    fun getUsers(blocked: ArrayList<Model>,blocker:ArrayList<Model>,currentUserLikeList:ArrayList<Model>)
 }
