bb4becc5c0941701f8336fbb1ba51a1f2b91cb94
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-04-24 19:58:05 +0300

    test have starter

--- app/src/main/java/com/kumsal/kyk/AdapterModel/post_adapter.kt
+++ app/src/main/java/com/kumsal/kyk/AdapterModel/post_adapter.kt
@@ -75,11 +75,11 @@ class post_adapter(private var list: ArrayList<post_model>, private var context:
             favoriteCount.setText(model.likesCount.toString())
             if (model.hasLiked!!){
                 favorite.setBtnColor(Color.RED);
-                favorite.setBtnFillColor(Color.GRAY)
+//                favorite.setBtnFillColor(Color.GRAY)
                 favorite.setAllowRandomColor(true)
             }else{
                 favorite.setBtnColor(Color.GRAY);
-                favorite.setBtnFillColor(Color.RED)
+//                favorite.setBtnFillColor(Color.RED)
                 favorite.setAllowRandomColor(true)
             }
             if (model.slider_adapter!=null){
