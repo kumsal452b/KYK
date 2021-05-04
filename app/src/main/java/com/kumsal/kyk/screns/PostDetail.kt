055b44e81ce39df9df8f691a6252f4cbca026e74
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-05-05 00:24:47 +0300

    problem have been not  solved

--- app/src/main/java/com/kumsal/kyk/screns/PostDetail.kt
+++ app/src/main/java/com/kumsal/kyk/screns/PostDetail.kt
@@ -7,37 +7,39 @@ import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import com.kumsal.kyk.AdapterModel.SliderImagePageAdapter
 import com.kumsal.kyk.R
 import com.kumsal.kyk.interfaces.imageCallback
 
-class PostDetail : AppCompatActivity(),imageCallback {
-    var pagerView:ViewPager?=null
-    var recyclerView:RecyclerView?=null
-    var commentContent:EditText?=null
-    var shareCommentBtn:Button?=null
-    var sharedImages:ArrayList<String>?=null
-    var adapter:SliderImagePageAdapter?=null
-    var animatedLayout:LinearLayout?=null
+open class PostDetail : AppCompatActivity(), imageCallback {
+    var pagerView: ViewPager? = null
+    var recyclerView: RecyclerView? = null
+    var commentContent: EditText? = null
+    var shareCommentBtn: Button? = null
+    var sharedImages: ArrayList<String>? = null
+    var adapter: SliderImagePageAdapter? = null
+    var animatedLayout: LinearLayout? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_detail)
-        recyclerView=findViewById(R.id.activity_post_detail_recyclerView)
-        pagerView=findViewById(R.id.activity_post_detail_pagerView)
-        commentContent=findViewById(R.id.activity_post_detail_edttext)
-        shareCommentBtn=findViewById(R.id.activity_post_detail_commentBtn)
-        animatedLayout=findViewById(R.id.activity_post_detail_animationLay)
+        recyclerView = findViewById(R.id.activity_post_detail_recyclerView)
+        pagerView = findViewById(R.id.activity_post_detail_pagerView)
+        commentContent = findViewById(R.id.activity_post_detail_edttext)
+        shareCommentBtn = findViewById(R.id.activity_post_detail_commentBtn)
+        animatedLayout = findViewById(R.id.activity_post_detail_animationLay)
 
-        animatedLayout?.startAnimation(AnimationUtils.loadAnimation(this,R.anim.placeholder))
-        sharedImages=intent.getStringArrayListExtra("images")
-        adapter=SliderImagePageAdapter(this,sharedImages)
+        animatedLayout?.startAnimation(AnimationUtils.loadAnimation(this, R.anim.placeholder))
+        sharedImages = intent.getStringArrayListExtra("images")
+        adapter = SliderImagePageAdapter(this, sharedImages)
         adapter?.setOnCallbackListener(this)
-        pagerView?.adapter=adapter
+        pagerView?.adapter = adapter
     }
 
     override fun imageLoadDoneCallback() {
-        animatedLayout?.visibility= View.GONE
+        this.animatedLayout?.isVisible = false
+        this.animatedLayout?.animation?.cancel()
     }
 }
