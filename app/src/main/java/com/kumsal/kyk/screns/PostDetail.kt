56b62d8ad67dad8e267faa75f3a2345405068c2e
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-05-14 20:15:24 +0300

    change somtihng

--- app/src/main/java/com/kumsal/kyk/screns/PostDetail.kt
+++ app/src/main/java/com/kumsal/kyk/screns/PostDetail.kt
@@ -18,6 +18,7 @@ import androidx.viewpager.widget.ViewPager
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.snapshot.StringNode
 import com.google.firebase.firestore.FirebaseFirestore
+import com.kumsal.kyk.AdapterModel.SendPostDataModel
 import com.kumsal.kyk.AdapterModel.SliderImagePageAdapter
 import com.kumsal.kyk.R
 import com.kumsal.kyk.interfaces.imageCallback
@@ -27,7 +28,7 @@ open class PostDetail : AppCompatActivity(), imageCallback {
     var recyclerView: RecyclerView? = null
     var commentContent: EditText? = null
     var shareCommentBtn: Button? = null
-    var sharedImages: ArrayList<String>? = null
+    var gettedPostArguman: SendPostDataModel? = null
     var adapter: SliderImagePageAdapter? = null
     var send_message: TextInputEditText? = null
     var fsReferenceForComment: FirebaseFirestore? = null
@@ -41,8 +42,8 @@ open class PostDetail : AppCompatActivity(), imageCallback {
         shareCommentBtn = findViewById(R.id.activity_post_detail_commentBtn)
         send_message = findViewById(R.id.activity_post_detail_msgText)
 
-        sharedImages = intent.getStringArrayListExtra("images")
-        adapter = SliderImagePageAdapter(this, sharedImages)
+        gettedPostArguman = intent.getParcelableExtra<SendPostDataModel>("images")!!
+        adapter = SliderImagePageAdapter(this, gettedPostArguman?.post_image_Url)
         adapter?.setOnCallbackListener(this)
         pagerView?.adapter = adapter
 
