b122611b86e1359275bfcdf5ae5ca5b022613ced
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-07-02 22:23:11 +0300

    add new class element

--- app/src/main/java/com/kumsal/kyk/Security/SecurityScreen.kt
+++ app/src/main/java/com/kumsal/kyk/Security/SecurityScreen.kt
@@ -15,5 +15,24 @@ class SecurityScreen {
     var allFriends:RadioButton?=null
     var expectFriedns:RadioButton?=null
     var toolbar:Toolbar?=null
-    
+    var accepted_selection_name:Button?=null
+    var select_all:CheckBox?=null
+
+    fun mainElement(){
+        recyclerElement = securityView?.findViewById(R.id.secure_recycler)!!
+        recyclerElement.setHasFixedSize(true)
+        recyclerElement.layoutManager = LinearLayoutManager(securityView.context)
+        mRadioGroup = securityView?.findViewById(R.id.secure_rg)
+        allFriends = securityView?.findViewById(R.id.secure_allfriends)
+        excpection = securityView?.findViewById(R.id.secure_except)
+        textForInfo = securityView?.findViewById(R.id.secure_bind_element_size)
+        textForInfo.measure(0, 0);       //must call measure!
+        toolbar = securityView.findViewById(R.id.secure_bind_toolbar)
+        accept_selected_name =
+            securityView.findViewById(R.id.secure_bind_accept)
+        selectedAll = securityView.findViewById(R.id.secure_bind_selectAll)
+        checkSecurePanel()
+        setSupportActionBar(toolbar)
+        recyclerView.adapter = mAdapter
+    }
 }
